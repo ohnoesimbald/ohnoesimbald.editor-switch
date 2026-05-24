6eacaf88e9b01732ecc3e81eb1304393f874c030
OhNoesImBald <ssseojun12344@gmail.com>
2026-05-23 21:40:56 -0600

    Add src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+using namespace geode::prelude;
+
+class $modify(MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+
+        auto mainMenu = this->getChildByID("main-menu");
+        if (!mainMenu) return true;
+
+        auto playBtn = mainMenu->getChildByID("play-button");
+        auto editorBtn = mainMenu->getChildByID("editor-button");
+
+        if (playBtn && editorBtn) {
+            auto playPos = playBtn->getPosition();
+            auto editorPos = editorBtn->getPosition();
+            playBtn->setPosition(editorPos);
+            editorBtn->setPosition(playPos);
+        }
+
+        return true;
+    }
+};

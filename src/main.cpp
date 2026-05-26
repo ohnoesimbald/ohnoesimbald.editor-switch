4941ff1b939a41d00a3c70cd66dd3e037d4ecaf3
OhNoesImBald <ssseojun12344@gmail.com>
2026-05-25 21:14:59 -0600

    Update main.cpp
    added scale and shit so it looks better

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,13 @@ class $modify(MenuLayer) {
         if (!mainMenu) return true;
         auto playBtn = mainMenu->getChildByID("play-button");
         auto editorBtn = mainMenu->getChildByID("editor-button");
-        if (playBtn && editorBtn && Mod::get()->getSettingValue<bool>("toggle")) {        //the button thingy that toggles the mod
+        if (playBtn && editorBtn && Mod::get()->getSettingValue<bool>("toggle")) { //the button thingy that toggles the mod
             auto playPos = playBtn->getPosition();
             auto editorPos = editorBtn->getPosition();
-            playBtn->setPosition(editorPos);          //makes the play button switch places with the editor
+            playBtn->setPosition(editorPos); //makes the play button switch places with the editor
             editorBtn->setPosition(playPos);
+            playBtn->setScale(0.8f); //scales the buttons and shit
+            editorBtn->setScale(1.2f);
         }
         return true;
     }

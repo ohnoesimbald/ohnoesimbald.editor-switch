3ea8ed2e99f3631a98147d6c7189aefeffe8c3e7
OhNoesImBald <ssseojun12344@gmail.com>
2026-06-04 17:03:35 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,43 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+
 using namespace geode::prelude;
 
 class $modify(MenuLayer) {
     bool init() {
-        if (!MenuLayer::init()) return false;
+        if (!MenuLayer::init())
+            return false;
+
         auto mainMenu = this->getChildByID("main-menu");
-        if (!mainMenu) return true;
+        if (!mainMenu)
+            return true;
+
         auto playBtn = mainMenu->getChildByID("play-button");
         auto editorBtn = mainMenu->getChildByID("editor-button");
-        if (playBtn && editorBtn && Mod::get()->getSettingValue<bool>("toggle")) { //the button thingy that toggles the mod
-            auto playPos = playBtn->getPosition();
-            auto editorPos = editorBtn->getPosition();
-            playBtn->setPosition(editorPos); //makes the play button switch places with the editor
+
+        if (playBtn && editorBtn && Mod::get()->getSettingValue<bool>("toggle")) {
+
+            // save positions
+            CCPoint playPos = playBtn->getPosition();
+            CCPoint editorPos = editorBtn->getPosition();
+
+            // reset scales first
+            playBtn->setScale(1.f);
+            editorBtn->setScale(1.f);
+
+            // swap positions
+            playBtn->setPosition(editorPos);
             editorBtn->setPosition(playPos);
-            playBtn->setScale(0.65f) //scalle button dipshit
-            editorBtn->setScale(0.5f) 
+
+            // THEN apply scaling
+            playBtn->setScale(0.65f);
+            editorBtn->setScale(0.5f);
+
+            // update visuals
+            playBtn->setContentSize(playBtn->getScaledContentSize());
+            editorBtn->setContentSize(editorBtn->getScaledContentSize());
         }
+
         return true;
     }
 };

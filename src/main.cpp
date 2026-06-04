e408ef317eee8c370e4c0df1f7de99e88763e340
OhNoesImBald <ssseojun12344@gmail.com>
2026-06-04 06:52:24 -0600

    Remove button scaling adjustments
    Removed scaling adjustments for play and editor buttons.

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,6 @@ class $modify(MenuLayer) {
             auto editorPos = editorBtn->getPosition();
             playBtn->setPosition(editorPos); //makes the play button switch places with the editor
             editorBtn->setPosition(playPos);
-            playBtn->setScale(0.5f); //scales the buttons and shit
-            editorBtn->setScale(1.5f);
         }
         return true;
     }
